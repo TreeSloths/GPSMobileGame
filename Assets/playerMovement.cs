56ea849df3a159ca5f434688c3123f1f77a0c13b
Emil Huzell <65963728+EmilHuzell@users.noreply.github.com>
2021-03-02 04:32:12 +0100

    Creates base prefab

---
+++ Assets/playerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    // Start is called before the first frame updat
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            transform.position += transform.forward * 0.1f;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position -= transform.forward * 0.1f;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position -= transform.right * 0.1f;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += transform.right * 0.1f;
+        }
+            
+    }
+}
