54ddd51ff041b56fae23b2fb1307a71bcef9ea1e
Adam Alzuhairi <adam_alzu@hotmail.com>
2021-03-04 09:27:11 +0100

    fixed database url problem and added firebase start script

---
+++ Assets/FirebaseStart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Firebase;
+using UnityEngine;
+
+public class FirebaseStart : MonoBehaviour
+{
+    public FirebaseApp FireBaseApp { get; set; }
+
+    private void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                FireBaseApp = FirebaseApp.DefaultInstance;
+            }
+
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                    "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+            }
+        });
+    }
+}
